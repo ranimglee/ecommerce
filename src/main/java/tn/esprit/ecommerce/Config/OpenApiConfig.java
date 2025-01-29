d92de159f61a187824d73f3ab83c61c62195d338
ranimglee <ranim.abassi@esprit.tn>
2025-01-29 12:09:42 +0100

    update package name

--- src/main/java/tn/esprit/ecommerce/Config/OpenApiConfig.java
+++ src/main/java/tn/esprit/ecommerce/Config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package tn.esprit.ecommerce.Config;
+package tn.esprit.ecommerce.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
